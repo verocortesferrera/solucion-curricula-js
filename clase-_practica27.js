cebf8252df8edc731a3f95eeefa2d97ee4f69039
verocortesferrera <32282690+verocortesferrera@users.noreply.github.com>
2017-09-27 23:11:04 -0300

    nuevo arreglo ejercicio

--- clase-_practica27.js
+++ clase-_practica27.js
@@ -3,19 +3,20 @@
 function range (variable1, variable2){
 	var star = [];
 	var end = [];
-	return [star + end];
+	
+	return star + end ;
 	}
 	
 
 function sum (array){
 	total = 0
 	for ( var i = 0 ; i < array.length; i++){
-		total += range[i];	
+		total += result[i];	
 	}
 	return total;
 }
 
-console.log(sum(range(1,10)))
+console.log(sum(range(1,10)));
 
 /* Ahora modifica la función de rango para tomar 
 un tercer argumento opcional que indique el valor de "step" 
@@ -29,3 +30,26 @@ de modo que range(5, 2, -1) produce [5, 4, 3, 2]. */
 
 
 // ejercicio 2
+/*Escribe una función que toma dos argumentos, el nombre de tu amiga 
+y la cantidad de puntos nuevos que ha obtenido, y que actualice los puntos 
+de la jugadora en el object scores. También escribe una función que imprima 
+en la consola el total de los puntos. */
+
+var scores = [['name','Marta']['points',0]];
+
+var argument = function(array2d){
+	var object = {};
+
+	for ( var i = 0; i < array2d.length; i ++){
+		object [array2d[i][0]] = array2d[i][1];
+	}
+	return object
+}
+
+console.log(argument(scores));
+
+
+
+scores[1][1] = 1500;
+
+console.log(argument(scores));
